7341d37e55ece48b7483cff4ea4662aee4ff8783
unknown <nguyenvantinh100999>
2024-05-20 15:25:32 +0700

    Tiva

---
+++ js/darklight.js
@@ -0,0 +1,13 @@
+document.getElementById("switchButton").onclick = function () {
+  document.getElementById("switchButton").classList.toggle("dark");
+  document.querySelector(".header").classList.toggle("dark");
+  document.querySelector(".carousel").classList.toggle("dark");
+  document.querySelector(".us").classList.toggle("dark");
+  document.querySelector(".w31-index3").classList.toggle("dark");
+  document.querySelector(".countup").classList.toggle("dark");
+  document.querySelector(".w3l-faq-block").classList.toggle("dark");
+  document.querySelector(".w3l-products").classList.toggle("dark");
+  document.querySelector(".w3l-testimonials").classList.toggle("dark");
+  document.querySelector(".build").classList.toggle("dark");
+  document.querySelector(".footer").classList.toggle("dark");
+};
